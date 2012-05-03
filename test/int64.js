32a9d8b4e967030707423a4fc504b0f33bfd77b6
Nathan Rajlich <nathan@tootallnate.net>
2012-05-02 17:36:16 -0700

    test: lots more tests

--- test/int64.js
+++ test/int64.js
@@ -4,6 +4,9 @@ var ref = require('../')
 
 describe('int64', function () {
 
+  var JS_MAX_INT = +9007199254740992
+  var JS_MIN_INT = -9007199254740992
+
   it('should allow simple ints to be written and read', function () {
     var buf = new Buffer(ref.sizeof.longlong)
     var val = 123456789
@@ -18,6 +21,49 @@ describe('int64', function () {
     ref.writeInt64(buf, 0, val)
     var rtn = ref.readInt64(buf, 0)
     assert.equal(val, rtn)
+  })
+
+  it('should return a Number when reading JS_MIN_INT', function () {
+    var buf = new Buffer(ref.sizeof.longlong)
+    ref.writeInt64(buf, 0, JS_MIN_INT)
+    var rtn = ref.readInt64(buf, 0)
+    assert.equal('number', typeof rtn)
+    assert.equal(JS_MIN_INT, rtn)
+  })
+
+  it('should return a Number when reading JS_MAX_INT', function () {
+    var buf = new Buffer(ref.sizeof.longlong)
+    ref.writeInt64(buf, 0, JS_MAX_INT)
+    var rtn = ref.readInt64(buf, 0)
+    assert.equal('number', typeof rtn)
+    assert.equal(JS_MAX_INT, rtn)
+  })
+
+  it('should return a String when reading JS_MAX_INT+1', function () {
+    var buf = new Buffer(ref.sizeof.longlong)
+    var plus_one = '9007199254740993'
+    ref.writeInt64(buf, 0, plus_one)
+    var rtn = ref.readInt64(buf, 0)
+    assert.equal('string', typeof rtn)
+    assert.equal(plus_one, rtn)
+  })
+
+  it('should return a String when reading JS_MIN_INT-1', function () {
+    var buf = new Buffer(ref.sizeof.longlong)
+    var minus_one = '-9007199254740993'
+    ref.writeInt64(buf, 0, minus_one)
+    var rtn = ref.readInt64(buf, 0)
+    assert.equal('string', typeof rtn)
+    assert.equal(minus_one, rtn)
+  })
+
+  it('should return a Number when reading 0, even when written as a String', function () {
+    var buf = new Buffer(ref.sizeof.longlong)
+    var zero = '0'
+    ref.writeInt64(buf, 0, zero)
+    var rtn = ref.readInt64(buf, 0)
+    assert.equal('number', typeof rtn)
+    assert.equal(0, rtn)
   })
 
 })
