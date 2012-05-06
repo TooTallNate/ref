fd864cd471e270128bcc816dfcc4910cb5d73eaf
Nathan Rajlich <nathan@tootallnate.net>
2012-05-06 14:15:54 -0700

    test: couple more string reading tests

--- test/string.js
+++ test/string.js
@@ -4,9 +4,21 @@ var ref = require('../')
 
 describe('C string', function () {
 
+  it('should return "" for a Buffer containing "\\0"', function () {
+    var buf = new Buffer('\0')
+    assert.strictEqual('', buf.readCString(0))
+  })
+
   it('should return "hello" for a Buffer containing "hello\\0world"', function () {
     var buf = new Buffer('hello\0world')
     assert.strictEqual('hello', buf.readCString(0))
+  })
+
+  it('should throw on a Buffer containing no \\0 byte', function () {
+    var buf = new Buffer('hello world')
+    assert.throws(function () {
+      buf.readCString(0)
+    })
   })
 
 })
