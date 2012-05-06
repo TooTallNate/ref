15b272bcb352d0af4ebec8b96e03b8c5a93bdfa5
Nathan Rajlich <nathan@tootallnate.net>
2012-05-05 18:24:56 -0700

    test: tweak

--- test/string.js
+++ test/string.js
@@ -2,11 +2,11 @@
 var assert = require('assert')
 var ref = require('../')
 
-describe('string', function () {
+describe('C string', function () {
 
   it('should return "hello" for a Buffer containing "hello\\0world"', function () {
     var buf = new Buffer('hello\0world')
-    assert.strictEqual('hello', buf.readCString())
+    assert.strictEqual('hello', buf.readCString(0))
   })
 
 })
