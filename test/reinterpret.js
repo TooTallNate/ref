f45f5551589518de825c13d14a0a21cd8525158e
Nathan Rajlich <nathan@tootallnate.net>
2012-05-09 21:37:34 -0700

    test: add reinterpret() reference retaining test

--- test/reinterpret.js
+++ test/reinterpret.js
@@ -1,5 +1,6 @@
 
 var assert = require('assert')
+var weak = require('weak')
 var ref = require('../')
 
 describe('reinterpret()', function () {
@@ -15,6 +16,30 @@ describe('reinterpret()', function () {
     assert.strictEqual(buf.address(), reinterpreted.address())
     assert.strictEqual(buf.length, reinterpreted.length)
     assert.strictEqual(buf.toString(), reinterpreted.toString())
+  })
+
+  it('should retain a reference to the original Buffer when reinterpreted', function () {
+    var origGCd = false
+    var otherGCd = false
+    var buf = new Buffer(1)
+    weak(buf, function () { origGCd = true })
+    var other = buf.reinterpret(0)
+    weak(other, function () { otherGCd = true })
+
+    assert(!origGCd, '"buf" has been garbage collected too soon')
+    assert(!otherGCd, '"other" has been garbage collected too soon')
+
+    // try to GC `buf`
+    buf = null
+    gc()
+    assert(!origGCd, '"buf" has been garbage collected too soon')
+    assert(!otherGCd, '"other" has been garbage collected too soon')
+
+    // now GC `other`
+    other = null
+    gc()
+    assert(otherGCd, '"other" has not been garbage collected')
+    assert(origGCd, '"buf" has not been garbage collected')
   })
 
 })
