7246c2b106ba82eacd128db63144a6fda4f6a456
Nathan Rajlich <nathan@tootallnate.net>
2012-05-17 14:25:19 -0700

    add another type coersion test

--- test/coerce.js
+++ test/coerce.js
@@ -11,7 +11,16 @@ describe('coerce', function () {
 
   it('should return a ref type when a "*" is present', function () {
     var type = ref.coerceType('void *')
+    assert(type !== ref.types.void)
     assert.equal(type.indirection, ref.types.void.indirection + 1)
+  })
+
+  it('should coerce the "type" property of a Buffer', function () {
+    var buf = new Buffer(ref.sizeof.int)
+    buf.type = 'int'
+    var type = ref.getType(buf)
+    assert.strictEqual(ref.types.int, type)
+    assert.strictEqual('int', buf.type)
   })
 
 })
