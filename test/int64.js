4e42bb95e51ca0b515fad7eaa2bd1e71f57b14ff
Nathan Rajlich <nathan@tootallnate.net>
2012-06-21 15:05:44 -0700

    implement the non-native-endianness read/write int64 functions

--- test/int64.js
+++ test/int64.js
@@ -78,4 +78,26 @@ describe('int64', function () {
     })
   })
 
+  ;['LE', 'BE'].forEach(function (endianness) {
+
+    describe(endianness, function () {
+
+      it('should read and write a signed ' + endianness + ' 64-bit integer', function () {
+        var val = -123456789
+        var buf = new Buffer(ref.sizeof.int64)
+        ref['writeInt64' + endianness](buf, 0, val)
+        assert.equal(val, ref['readInt64' + endianness](buf, 0))
+      })
+
+      it('should read and write an unsigned ' + endianness + ' 64-bit integer', function () {
+        var val = 123456789
+        var buf = new Buffer(ref.sizeof.uint64)
+        ref['writeUInt64' + endianness](buf, 0, val)
+        assert.equal(val, ref['readUInt64' + endianness](buf, 0))
+      })
+
+    })
+
+  })
+
 })
