a78c68225d2377606c8e76e5014da71a514b418d
Nathan Rajlich <nathan@tootallnate.net>
2012-07-12 00:56:42 -0700

    docs: fix gh-pages script

--- docs/gh-pages.sh
+++ docs/gh-pages.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+DIR=`dirname $0`
+
 npm run docs \
   && rm -rf /tmp/ref_docs \
   && mkdir -p /tmp/ref_docs \
-  && cp -Lrf {index.html,images,scripts,stylesheets} /tmp/ref_docs \
+  && cp -Lrf {"$DIR/index.html","$DIR/images","$DIR/scripts","$DIR/stylesheets"} /tmp/ref_docs \
   && git checkout gh-pages \
   && cp -Lrf /tmp/ref_docs/* . \
   && echo "done"
