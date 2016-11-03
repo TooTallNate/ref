6fdb4b7b234d527eb4aad57943797d8aabf0bdd2
Michael Hertsch <michael.hertsch@interactivedata.com>
2016-11-03 15:27:30 +0100

    Replaced ForceSet with Nan::ForceSet to remove deprecation warnings when using node 6.9.1

--- src/binding.cc
+++ src/binding.cc
@@ -640,8 +640,8 @@ NAN_MODULE_INIT(init) {
   // exports
   target->Set(Nan::New<v8::String>("sizeof").ToLocalChecked(), smap);
   target->Set(Nan::New<v8::String>("alignof").ToLocalChecked(), amap);
-  target->ForceSet(Nan::New<v8::String>("endianness").ToLocalChecked(), Nan::New<v8::String>(CheckEndianness()).ToLocalChecked(), static_cast<PropertyAttribute>(ReadOnly|DontDelete));
-  target->ForceSet(Nan::New<v8::String>("NULL").ToLocalChecked(), WrapNullPointer(), static_cast<PropertyAttribute>(ReadOnly|DontDelete));
+  Nan::ForceSet(target, Nan::New<v8::String>("endianness").ToLocalChecked(), Nan::New<v8::String>(CheckEndianness()).ToLocalChecked(), static_cast<PropertyAttribute>(ReadOnly|DontDelete));
+  Nan::ForceSet(target, Nan::New<v8::String>("NULL").ToLocalChecked(), WrapNullPointer(), static_cast<PropertyAttribute>(ReadOnly|DontDelete));
   Nan::SetMethod(target, "address", Address);
   Nan::SetMethod(target, "hexAddress", HexAddress);
   Nan::SetMethod(target, "isNull", IsNull);
