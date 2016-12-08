615016ac1a916f72e0b0e338da58048992b0d48b
Nathan Rajlich <nathan@tootallnate.net>
2016-12-08 15:35:12 -0800

    test: fix `util.inspect()` test
    Actually *use* util.inspect(), so that it properly fails
    on Node v7

--- test/address.js
+++ test/address.js
@@ -1,6 +1,6 @@
-
-var assert = require('assert')
 var ref = require('../')
+var assert = require('assert')
+var inspect = require('util').inspect
 
 describe('address', function () {
 
@@ -44,7 +44,7 @@ describe('address', function () {
   describe('inspect()', function () {
 
     it('should overwrite the default Buffer#inspect() to print the memory address', function () {
-      assert(buf.inspect().indexOf(buf.hexAddress()) !== -1)
+      assert(inspect(buf).indexOf(buf.hexAddress()) !== -1)
     })
 
   })
