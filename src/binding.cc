7aee9988d0f67199ebdb42075e20409b5ecbbe21
Ben Davies <kaiepi@outlook.com>
2018-03-25 09:24:21 -0300

    Implement support for wchar_t

--- src/binding.cc
+++ src/binding.cc
@@ -604,6 +604,7 @@ NAN_MODULE_INIT(init) {
   SET_SIZEOF(ulonglong, unsigned long long);
   SET_SIZEOF(pointer, char *);
   SET_SIZEOF(size_t, size_t);
+  SET_SIZEOF(wchar_t, wchar_t);
   // size of a Persistent handle to a JS object
   SET_SIZEOF(Object, Nan::Persistent<Object>);
 
@@ -635,6 +636,7 @@ NAN_MODULE_INIT(init) {
   SET_ALIGNOF(ulonglong, unsigned long long);
   SET_ALIGNOF(pointer, char *);
   SET_ALIGNOF(size_t, size_t);
+  SET_ALIGNOF(wchar_t, wchar_t);
   SET_ALIGNOF(Object, Nan::Persistent<Object>);
 
   // exports
